251537f48d079c7c84a332b90f31fc1a49004ad0
Will Boyle <willboyle18@gmail.com>
2025-08-04 16:41:28 -0700

    Turn single threaded server into multi-threaded server

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{BufReader, prelude::*};
 use std::fs;
+use std::thread;
+use rust_web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(||{
+            handle_connection(stream);
+        });
     }
 }
 
